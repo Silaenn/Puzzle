057e6542f41a9aa3b228316b43037f7aaceaa104
Deo silaen <deokeldisilaen@gmail.com>
2025-03-10 20:43:49 +0700

    update MainMenu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,17 +1,40 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     public AudioSource audioSource;
+    public Slider sfxSlider;
     public AudioClip dragSound;
     public AudioClip dropSound;
     public AudioClip correctSound;
     public AudioClip wrongSound;
+    public AudioClip clickSound;
+
 
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        float savedVolume = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        if (sfxSlider != null)
+        {
+            sfxSlider.value = savedVolume;
+            sfxSlider.onValueChanged.AddListener(SetVolume);
+        }
+        SetVolume(savedVolume);
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
     }
 
     public void PlayDragSound()
@@ -24,6 +47,7 @@ public class AudioManager : MonoBehaviour
         audioSource.PlayOneShot(dropSound);
     }
 
+
     public void PlayCorrectSound()
     {
         audioSource.PlayOneShot(correctSound);
@@ -32,5 +56,10 @@ public class AudioManager : MonoBehaviour
     public void PlayWrongSound()
     {
         audioSource.PlayOneShot(wrongSound);
+    }
+
+    public void PlayClickSound()
+    {
+        audioSource.PlayOneShot(clickSound);
     }
 }
--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BGMManager : MonoBehaviour
 {
     public static BGMManager instance;
     public AudioSource audioSource;
     public AudioClip bgmClip;
+    public Slider volumeSlider;
 
     private void Awake()
     {
@@ -19,10 +21,30 @@ public class BGMManager : MonoBehaviour
         }
     }
 
-    private void Start()
+    void Start()
     {
         audioSource.clip = bgmClip;
         audioSource.loop = true;
+
+        if (PlayerPrefs.HasKey("BGMVolume"))
+        {
+            float savedVolume = PlayerPrefs.GetFloat("BGMVolume");
+            audioSource.volume = savedVolume;
+            if (volumeSlider != null) volumeSlider.value = savedVolume;
+        }
+
         audioSource.Play();
+
+        if (volumeSlider != null)
+        {
+            volumeSlider.onValueChanged.AddListener(SetVolume);
+        }
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+        PlayerPrefs.SetFloat("BGMVolume", volume);
+        PlayerPrefs.Save();
     }
 }
